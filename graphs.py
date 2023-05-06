fix loss computation, replaced relu with leakyrelu

graphs.py
@@ -8,9 +8,7 @@ class Graphs:
         self.losses = np.array([], dtype=np.float32)
 
     def plot_rewards(self):
-        terminated_indices = np.array(np.where(self.rewards == -1)).reshape(-1) + 1
-        reward_series = np.array_split(self.rewards, terminated_indices)
-        reward_series = list(map(lambda rewards: rewards.sum(), reward_series))
+        reward_series = self.get_series_reward()
         plt.plot(reward_series)
         plt.xlabel('Series number')
         plt.ylabel('Reward')
@@ -19,6 +17,12 @@ class Graphs:
         plt.plot(self.losses)
         plt.xlabel('Epoch')
         plt.ylabel('Loss')
+
+    def get_series_reward(self):
+        terminated_indices = np.array(np.where(self.rewards == -1)).reshape(-1) + 1
+        reward_series = np.array_split(self.rewards, terminated_indices)
+        reward_series = list(map(lambda rewards: rewards.sum(), reward_series))
+        return reward_series
 
     def push_loss(self, loss):
         self.losses = np.append(self.losses, loss)

network.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import random
+import torch.nn.functional as F
 from torch import nn
 from collections import deque, namedtuple
 
@@ -8,27 +9,36 @@ from game import GameEnviroment
 from graphs import Graphs
 from config import *
 
+LR = 0.0001
+EPSILON = 0.8
+GAMMA = 0.9
+BATCH_SIZE = 2
+EPOCHS = 10000
+TARGET_UPDATE_FREQUENCY = 10
+MEMORY_SIZE = 200
+
 
 class DQN(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=96, kernel_size=(7, 7), stride=(1, 1)),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.MaxPool2d(5),
 
             nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(3, 3), stride=(1, 1)),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.MaxPool2d(3),
 
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), stride=(1, 1)),
-            nn.ReLU(),
+            nn.LeakyReLU(),
             nn.MaxPool2d(3),
 
             nn.Flatten(),
 
-            nn.Linear(in_features=131072, out_features=2048),
-            nn.ReLU(),
+            nn.Linear(in_features=25088, out_features=2048),
+            nn.BatchNorm1d(2048),
+            nn.LeakyReLU(),
             nn.Linear(in_features=2048, out_features=4),
         )
 
@@ -52,14 +62,14 @@ class Memory:
 
 
 Transition = namedtuple('Transition', ('state', 'n_state', 'action', 'reward', 'terminated'))
-memory = Memory(2000)
+memory = Memory(MEMORY_SIZE)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 predict_network = DQN().to(device)
 target_network = DQN().to(device)
 huber_loss = nn.HuberLoss()
-optimizer = torch.optim.Adam(predict_network.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(predict_network.parameters(), lr=LR)
 possible_actions = [0, 1, 2, 3]
 
 game = GameEnviroment(SCREEN_SIZE, FPS)
@@ -81,11 +91,11 @@ def epsilon_greedy_policy(epsilon, state):
 
 
 def do_one_step():
-    state = torch.tensor(game.get_state()[np.newaxis].reshape((1, 3, 800, 800)), dtype=torch.float32).to(device)
-    action = epsilon_greedy_policy(0.9, state)
+    state = torch.tensor(game.get_state()[np.newaxis].reshape((1, 3, 400, 400)), dtype=torch.float32).to(device)
+    action = epsilon_greedy_policy(EPSILON, state)
 
     reward, next_state, terminated = game.step(action)
-    next_state = torch.tensor(next_state[np.newaxis].reshape((1, 3, 800, 800)), dtype=torch.float32)
+    next_state = torch.tensor(next_state[np.newaxis].reshape((1, 3, 400, 400)), dtype=torch.float32)
 
     graphs.push_reward(reward)
     memory.push(Transition(state, next_state, action, reward, terminated))
@@ -97,21 +107,26 @@ def training_step(batch_size, gamma):
         return
 
     states, next_states, actions, rewards, terminated = Transition(*zip(*memory.sample(batch_size)))
+    actions = torch.tensor(actions).to(device)
     rewards = torch.tensor(rewards).to(device)
     terminated = torch.tensor(terminated).to(device)
     states = torch.stack(states).squeeze(1).to(device)
+    next_states = torch.stack(next_states).squeeze(1).to(device)
 
-    estimated_q_values = predict_network(states).max(axis=1).values
+    mask = F.one_hot(actions, len(possible_actions))
+    q_values = predict_network(states)
+    q_values = torch.sum(q_values * mask, dim=1)
 
-    target_q_values = rewards + (1 - terminated) * gamma * estimated_q_values
+    with torch.no_grad():
+        next_state_q_values = target_network(next_states).max(axis=1).values
+        target_q_values = rewards + (1 - terminated) * gamma * next_state_q_values
 
-    loss = huber_loss(estimated_q_values, target_q_values)
+    loss = huber_loss(q_values, target_q_values)
 
     optimizer.zero_grad()
     loss.backward()
+    torch.nn.utils.clip_grad_value_(predict_network.parameters(), 100)
     optimizer.step()
-
-    target_network.load_state_dict(predict_network.state_dict())
 
     graphs.push_loss(loss.item())
     print(f'Loss: {loss.item()}')
@@ -121,9 +136,14 @@ def training_loop(epochs, batch_size):
     for epoch in range(epochs):
         print(f'======== {epoch + 1}/{epochs} epoch')
         do_one_step()
-        training_step(batch_size, 0.9)
+        training_step(batch_size, GAMMA)
+
+        if epoch % TARGET_UPDATE_FREQUENCY == 0:
+            target_network.load_state_dict(predict_network.state_dict())
+
+        print(graphs.get_series_reward())
         graphs.plot_rewards()
         graphs.plot_loss()
 
 
-training_loop(30, 1)
+training_loop(EPOCHS, BATCH_SIZE)