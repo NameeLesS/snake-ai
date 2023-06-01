4eca893ae0a0d629d5569ff4d7eb870b191019c9
NameeLesS <marcin.dziobkowski01@gmail.com>
2023-06-01 18:28:14 +0200

    add MemoryManagmentProcess

--- distributed.py
+++ distributed.py
@@ -1,12 +1,15 @@
+import multiprocessing.connection
+
 import torch
 import numpy as np
 import traceback
 from torch import nn
-from multiprocessing import Process, Manager, Pipe
+from multiprocessing import Process, Value, Pipe
 
 from game import GameEnviroment
 from memory import PrioritizedReplayBuffer
 from metrics import TrainMatrics
+from network import DQN
 from config import *
 
 # General constants
@@ -25,43 +28,7 @@ MEMORY_SIZE = 200
 ALPHA = 0.9
 BETA = 0.4
 
-
-class DQN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(7, 7), stride=(1, 1)),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(5),
-
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(3),
-
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1)),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(3),
-
-            nn.Flatten(),
-
-            nn.Linear(in_features=1024, out_features=128),
-            nn.BatchNorm1d(128),
-            nn.LeakyReLU(),
-            nn.Linear(in_features=128, out_features=4),
-        )
-
-    def forward(self, x):
-        x = x / 255
-        x = self.model(x)
-        return x
-
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-memory = PrioritizedReplayBuffer(buffer_size=MEMORY_SIZE, state_dim=STATE_DIM, action_dim=(1), alpha=ALPHA, beta=BETA)
 predict_network = DQN().to(device)
 target_network = DQN().to(device)
 target_network.load_state_dict(predict_network.state_dict())
@@ -69,19 +36,16 @@ target_network.load_state_dict(predict_network.state_dict())
 optimizer = torch.optim.AdamW(predict_network.parameters(), lr=LR, amsgrad=True)
 possible_actions = [0, 1, 2, 3]
 
-game = GameEnviroment(SCREEN_SIZE, FPS, False)
-game.execute()
 metrics = TrainMatrics()
 
 
-def training_step(batch_size, gamma):
-    if len(memory) < batch_size:
-        return
-
+def training_step(batch_size, gamma, data_update, samples):
     predict_network.train()
     target_network.train()
+    ready_connections = multiprocessing.connection.wait([samples])
 
-    batch, weights, tree_idxs = memory.sample(batch_size)
+    if samples in ready_connections:
+        batch, weights, tree_idxs = samples.recv()
     states, next_states, actions, rewards, terminated = batch
     states = states.to(torch.float32).to(device)
     next_states = next_states.to(torch.float32).to(device)
@@ -98,7 +62,7 @@ def training_step(batch_size, gamma):
     huber = nn.SmoothL1Loss(reduce=lambda x: torch.mean(x * weights))
     loss = huber(q_values, target_q_values.unsqueeze(1))
     td_error = torch.abs(q_values.squeeze(1) - target_q_values)
-    memory.update_priorities(data_idxs=tree_idxs, priorities=td_error)
+    data_update.send({'idxs': tree_idxs, 'priorities': td_error})
 
     optimizer.zero_grad()
     loss.backward()
@@ -108,10 +72,14 @@ def training_step(batch_size, gamma):
     metrics.push_loss(loss.item())
 
 
-def training_loop(epochs, batch_size, network, data_receiver, terminated):
+def training_loop(epochs, batch_size, network, data_update, samples, memory_size, sample_req, terminated):
+    while int(memory_size.value) < batch_size:
+        pass
+
     for epoch in range(epochs):
         print(f'======== {epoch + 1}/{epochs} epoch ========')
-        training_step(batch_size, GAMMA)
+        sample_req.value = True
+        training_step(batch_size, GAMMA, data_update, samples)
         metrics.calculate()
 
         print(f'Loss: {metrics.loss}')
@@ -119,18 +87,52 @@ def training_loop(epochs, batch_size, network, data_receiver, terminated):
         if epoch % TARGET_UPDATE_FREQUENCY == 0:
             target_network.load_state_dict(predict_network.state_dict())
 
-        for step in range(STEPS):
-            memory.add(data_receiver.recv())
-        network.clear()
-        network.update(predict_network.state_dict())
+        network.send(predict_network.state_dict())
+
     terminated.value = True
 
 
+class MemoryManagmentProcess(Process):
+    def __init__(self, data, data_updates, samples, memory_size, sample_req, terminated):
+        Process.__init__(self)
+        self.memory = PrioritizedReplayBuffer(
+            buffer_size=MEMORY_SIZE,
+            state_dim=STATE_DIM,
+            action_dim=(1),
+            alpha=ALPHA, beta=BETA
+        )
+
+        self.data = data
+        self.data_updates = data_updates
+        self.samples = samples
+        self.terminated = terminated
+        self.memory_size = memory_size
+        self.sample_req = sample_req
+
+    def update_priorities(self):
+        update = self.data_updates.recv()
+        self.memory.update_priorities(data_idxs=update['idxs'], priorities=update['priorities'])
+
+    def run(self):
+        while True:
+            self.memory_size.value = len(self.memory)
+            if self.data.poll():
+                self.memory.add(self.data.recv())
+            if self.data_updates.poll():
+                self.update_priorities()
+            if self.sample_req.value:
+                self.samples.send(self.memory.sample(BATCH_SIZE))
+                self.sample_req.value = False
+            if self.terminated.value:
+                break
+            print(f'memory managment {self.memory_size}')
+
+
 class DataCollectionProcess(Process):
-    def __init__(self, network, data_sender, terminated):
+    def __init__(self, network, data, terminated):
         Process.__init__(self)
         self.network = network
-        self.data_sender = data_sender
+        self.data = data
         self.terminated = terminated
         self.game = GameEnviroment(SCREEN_SIZE, FPS, False)
         self.predict_network = DQN()
@@ -166,24 +168,53 @@ class DataCollectionProcess(Process):
             while True:
                 if self.terminated.value:
                     break
-                if self.network:
-                    self.predict_network.load_state_dict(self.network)
+                if self.network.poll():
+                    nt = self.network.recv()
+                    self.predict_network.load_state_dict(nt)
+
                 data, terminated = self.do_one_step(0.1)
-                self.data_sender.send(data)
-            print('end')
+                self.data.send(data)
+                print('collection')
         except:
             traceback.print_exc()
 
 
 def main():
-    with Manager() as manager:
-        network = manager.dict(predict_network.state_dict())
-        terminated = manager.Value('b', False)
-        data_recv, data_sender = Pipe()
+    network_recv, network_sender = Pipe()
+    data_recv, data_sender = Pipe()
+    data_updates_recv, data_updated_sender = Pipe()
+    samples_recv, samples_sender = Pipe()
+    network_sender.send(predict_network.state_dict())
+    terminated = Value('b', False)
+    memory_size = Value('i', 0)
+    sample_req = Value('b', False)
 
-        data_collection_process = DataCollectionProcess(network, data_sender, terminated)
-        data_collection_process.start()
-        training_loop(EPOCHS, BATCH_SIZE, network, data_recv, terminated)
+    data_collection_process = DataCollectionProcess(
+        network_recv,
+        data_sender,
+        terminated
+    )
+
+    memory_managment_process = MemoryManagmentProcess(
+        data_recv,
+        data_updates_recv,
+        samples_sender,
+        memory_size,
+        sample_req,
+        terminated
+    )
+
+    data_collection_process.start()
+    memory_managment_process.start()
+
+    training_loop(EPOCHS,
+                  BATCH_SIZE,
+                  network_sender,
+                  data_updated_sender,
+                  samples_recv,
+                  memory_size,
+                  sample_req,
+                  terminated)
 
 
 if __name__ == '__main__':
