850e4f96a481cad01a742006c4187b3a24baa444
NameeLesS <marcin.dziobkowski01@gmail.com>
2023-06-03 14:48:36 +0200

    data collection process collects now specified number of frames per epoch, code refactoring

--- distributed.py
+++ distributed.py
@@ -18,6 +18,8 @@ GAMMA = 0.99
 BATCH_SIZE = 8
 EPOCHS = 200
 TARGET_UPDATE_FREQUENCY = 100
+STEPS = 50
+DECAY_RATE_CHANGE = 7e-6
 
 # Memory constants
 MEMORY_SIZE = 200
@@ -28,19 +30,25 @@ possible_actions = [0, 1, 2, 3]
 
 
 class TrainingProcess:
-    def __init__(self, predict_network, target_network, device):
+    def __init__(self, predict_network, target_network, device, network, data_updates, samples, memory_size, sample_req, terminated):
         self.predict_network = predict_network
         self.target_network = target_network
         self.device = device
+        self.network = network
+        self.data_updates = data_updates
+        self.samples = samples
+        self.memory_size = memory_size
+        self.sample_req = sample_req
+        self.terminated = terminated
+
         self.optimizer = torch.optim.AdamW(self.predict_network.parameters(), lr=LR, amsgrad=True)
         self.metrics = TrainMatrics()
 
-    def training_step(self, gamma, data_update, samples):
+    def training_step(self, gamma, sample):
         self.predict_network.train()
         self.target_network.train()
 
-        if samples.poll(timeout=999):
-            batch, weights, tree_idxs = samples.recv()
+        batch, weights, tree_idxs = sample
         states, next_states, actions, rewards, terminated = batch
         states = states.to(torch.float32).to(self.device)
         next_states = next_states.to(torch.float32).to(self.device)
@@ -57,31 +65,34 @@ class TrainingProcess:
         huber = nn.SmoothL1Loss(reduce=lambda x: torch.mean(x * weights))
         loss = huber(q_values, target_q_values.unsqueeze(1))
         td_error = torch.abs(q_values.squeeze(1) - target_q_values)
-        data_update.send({'idxs': tree_idxs, 'priorities': td_error.detach()})
 
         self.optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_value_(self.predict_network.parameters(), 100)
         self.optimizer.step()
 
-        self.metrics.push_loss(loss.item())
+        return tree_idxs, td_error, loss.item()
 
-    def training_loop(self, epochs, batch_size, data_update, samples, memory_size, sample_req, terminated):
-        while int(memory_size.value) < batch_size:
+    def training_loop(self, epochs, batch_size, gamma):
+        while int(self.memory_size.value) < batch_size:
             pass
 
         for epoch in range(epochs):
             print(f'======== {epoch + 1}/{epochs} epoch ========')
-            sample_req.value = True
-            self.training_step(GAMMA, data_update, samples)
-            self.metrics.calculate()
+            self.sample_req.value = True
+            if self.samples.poll(timeout=999):
+                sample = self.samples.recv()
 
-            print(f'Loss: {self.metrics.loss}')
+                tree_idxs, td_error, loss = self.training_step(gamma, sample)
 
-            if epoch % TARGET_UPDATE_FREQUENCY == 0:
-                self.target_network.load_state_dict(self.predict_network.state_dict())
+                print(f'Loss: {loss}')
 
-        terminated.value = True
+                if epoch % TARGET_UPDATE_FREQUENCY == 0:
+                    self.target_network.load_state_dict(self.predict_network.state_dict())
+
+                self.data_updates.send({'idxs': tree_idxs, 'priorities': td_error.detach()})
+                self.network.send(self.predict_network.state_dict())
+        self.terminated.value = True
 
 
 class MemoryManagmentProcess(Process):
@@ -120,6 +131,9 @@ class MemoryManagmentProcess(Process):
             self.samples.send(self.memory.sample(BATCH_SIZE))
             self.sample_req.value = False
 
+    def update_metrics(self):
+        pass
+
     def save_metrics(self):
         self.metrics.save('', 'metrics.pkl')
 
@@ -130,6 +144,7 @@ class MemoryManagmentProcess(Process):
             self.update_data()
             self.update_priorities()
             self.send_sample()
+            self.update_metrics()
 
             if self.terminated.value:
                 break
@@ -140,7 +155,8 @@ class MemoryManagmentProcess(Process):
 class DataCollectionProcess(Process):
     def __init__(self, network, data, device, terminated):
         Process.__init__(self)
-        self.predict_network = network
+        self.predict_network = DQN().to(device)
+        self.network = network
         self.data = data
         self.terminated = terminated
         self.device = device
@@ -173,12 +189,19 @@ class DataCollectionProcess(Process):
 
     def run(self):
         self.game.execute()
+        i = 0
         while True:
             if self.terminated.value:
                 break
 
-            data = self.do_one_step(0.1)
-            self.data.send(data)
+            if self.network.poll(timeout=999):
+                self.predict_network.load_state_dict(self.network.recv())
+                for step in range(STEPS):
+                    epsilon = 1 * (1 - DECAY_RATE_CHANGE) ** i
+                    data = self.do_one_step(epsilon)
+                    self.data.send(data)
+
+            i += 1
 
 
 def main():
@@ -186,15 +209,10 @@ def main():
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     predict_network = DQN().to(device)
-    predict_network.share_memory()
     target_network = DQN().to(device)
     target_network.load_state_dict(predict_network.state_dict())
 
-    training_process = TrainingProcess(
-        predict_network=predict_network,
-        target_network=target_network,
-        device=device)
-
+    network_recv, network_sender = Pipe()
     data_recv, data_sender = Pipe()
     data_updates_recv, data_updated_sender = Pipe()
     samples_recv, samples_sender = Pipe()
@@ -202,32 +220,40 @@ def main():
     memory_size = Value('i', 0)
     sample_req = Value('b', False)
 
+    network_sender.send(predict_network.state_dict())
+
+    training_process = TrainingProcess(
+        predict_network=predict_network,
+        target_network=target_network,
+        device=device,
+        network=network_sender,
+        data_updates=data_updated_sender,
+        samples=samples_recv,
+        memory_size=memory_size,
+        sample_req=sample_req,
+        terminated=terminated
+    )
+
     data_collection_process = DataCollectionProcess(
-        predict_network,
-        data_sender,
-        device,
-        terminated
+        network=network_recv,
+        data=data_sender,
+        device=device,
+        terminated=terminated
     )
 
     memory_managment_process = MemoryManagmentProcess(
-        data_recv,
-        data_updates_recv,
-        samples_sender,
-        memory_size,
-        sample_req,
-        terminated
+        data=data_recv,
+        data_updates=data_updates_recv,
+        samples=samples_sender,
+        memory_size=memory_size,
+        sample_req=sample_req,
+        terminated=terminated
     )
 
     data_collection_process.start()
     memory_managment_process.start()
 
-    training_process.training_loop(EPOCHS,
-                                   BATCH_SIZE,
-                                   data_updated_sender,
-                                   samples_recv,
-                                   memory_size,
-                                   sample_req,
-                                   terminated)
+    training_process.training_loop(EPOCHS, BATCH_SIZE, GAMMA)
 
 
 if __name__ == '__main__':
